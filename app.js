63d4d450b737f64dd7f22f06d62af8fadcd35f72
Marcela Maciel <macielwebcode@gmail.com>
2024-11-21 12:00:36 -0300

    adicionado cors

--- app.js
+++ app.js
@@ -5,12 +5,31 @@ dotenv.config()
 import './src/database';
 
 import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet';
+
 import homeRoutes from './src/routes/homeRoutes'
 import userRoutes from './src/routes/userRoutes'
 import tokenRoutes from './src/routes/tokenRoutes'
 import demandaRoutes from './src/routes/demandaRoutes'
 import fotoRoutes from './src/routes/fotoRoutes'
 import customerRoutes from './src/routes/customerRoutes'
+
+//colocar aqui os dominios permitidos
+const whiteList = [
+    'http://localhost:3000'
+]
+
+//origin é setado pelo browser
+const corsOptions = {
+    origin: function(origin, callback){
+        if(whiteList.indexOf(origin) !== -1 || !origin){
+            callback(null, true)
+        }else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 class App{
     constructor(){
@@ -20,8 +39,11 @@ class App{
     }
 
     middlewares(){
+        this.app.use(cors(corsOptions))
+        this.app.use(helmet())
         this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.json())
+        // this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads, images')))
     }
 
     routes(){
